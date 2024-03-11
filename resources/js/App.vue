8d94dec6aa785358a66261dd5bb749e882921ce1
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-03-11 18:15:41 +0300

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="container text-3xl font-bold underline text-orange-500 text-center">Hello from Vue 3!</div>
+</template>
+
+<style scoped>
+
+</style>
